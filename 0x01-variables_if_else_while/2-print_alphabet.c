571ba706466c27a9e5fcc0f62c1a8589cc56241d
Yusuf Mary Oluwatobiloba <omotolaniyusuph@gmail.com>
2023-06-17 15:30:33 -0700

    shell variable

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+
 /**
-* main - Prints the alphabetic
+* main - Prints the alphabet.
 *
-* Return: Always (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
- 
+
