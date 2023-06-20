44c161a4e0d39c398350325f88b898d0dcb0178f
Yusuf Mary Oluwatobiloba <omotolaniyusuph@gmail.com>
2023-06-20 07:01:33 -0700

    loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
-#include "stdio.h"
+#include "main.h"
+#include "unistd.h"
+
 /**
 * main - Entry point
 *
@@ -6,16 +8,17 @@
 */
 int main(void)
 {
-	putchar('H');
-	putchar('o');
-	putchar('l');
-	putchar('b');
-	putchar('e');
-	putchar('r');
-	putchar('t');
-	putchar('o');
-	putchar('n');
-	putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
 
