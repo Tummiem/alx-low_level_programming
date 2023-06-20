71db429cb7f8f9259e45e0976ce3c917fa31976e
Yusuf Mary Oluwatobiloba <omotolaniyusuph@gmail.com>
2023-06-20 15:29:25 -0700

    looped

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-#include "stdio.h"
 /**
-* main - Entry point
-*
-* Return: Always 0
+* main - print _putchar
+* Return: 0
 */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
